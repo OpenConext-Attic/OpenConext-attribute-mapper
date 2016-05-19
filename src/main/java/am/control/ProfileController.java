7d159cb64687c094ba9e5aa3203eee6f87838851
oharsta <oharsta@zilverline.com>
2016-05-19 15:34:29 +0200

    Add ID to profile

--- src/main/java/am/control/ProfileController.java
+++ src/main/java/am/control/ProfileController.java
@@ -3,6 +3,7 @@ package am.control;
 import am.domain.User;
 import am.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,11 +19,18 @@ import java.util.Optional;
 @Controller
 public class ProfileController {
 
+  @Autowired
+  private Environment environment;
+
   @RequestMapping("/profile")
   public String profile(Authentication authentication, ModelMap modelMap) {
     modelMap.put("menu", "profile");
     modelMap.put("user", authentication.getPrincipal());
+    modelMap.put("environment", environment.acceptsProfiles("test"));
     return "profile";
   }
 
+  public void setEnvironment(Environment environment) {
+    this.environment = environment;
+  }
 }
--- src/test/java/am/control/ProfileControllerTest.java
+++ src/test/java/am/control/ProfileControllerTest.java
@@ -1,18 +1,26 @@
 package am.control;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.ui.ModelMap;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 
 public class ProfileControllerTest {
 
   private ProfileController subject = new ProfileController();
 
+  @Before
+  public void before() throws NoSuchFieldException {
+    subject.setEnvironment(mock(Environment.class));
+  }
+
   @Test
   public void testProfile() throws Exception {
-      String view = subject.profile(new TestingAuthenticationToken("principal", "N/A"), new ModelMap());
-      assertEquals("profile", view);
+    String view = subject.profile(new TestingAuthenticationToken("principal", "N/A"), new ModelMap());
+    assertEquals("profile", view);
   }
 }
